3d759371cde29c11425a607b95d71d1d776ec29b
Christian Hirsch <hirsch@math.lmu.de>
2017-04-25 16:25:44 +1000

    Add notebooks

--- README.md
+++ README.md
@@ -12,23 +12,16 @@ https://www.continuum.io/downloads
 
 Die Grundlage fuer unseren Kurs bildet Googles Programmiersprache [Tensorflow](https://www.tensorflow.org/). Bei bereits eingerichtetem [Anaconda](https://www.continuum.io/downloads) ist die Installation von Tensorflow ein Kinderspiel:
 
-https://www.tensorflow.org/versions/r0.11/get_started/os_setup#anaconda_installation
+
+https://www.tensorflow.org/install/install_windows#installing_with_anaconda
 
 
-## Installation von Keras
+## Installationsüberpruefung und Laden vortrainierter Datensätze
 
-Tensorflow bietet weitreichende Flexibilitaeten bei der Konzeption eigener neuronaler Netze. Fuer das schnelle Bauen von Prototypen genuegt es aber oft vorhandene Standard-Bausteine zusammensetzen. Diese Funktion bietet [Keras](https://keras.io/). Die Installation von Keras bei bestehendem Anaconda/Pip ist unter Punkt 5 beschrieben:
-
-http://machinelearningmastery.com/setup-python-environment-machine-learning-deep-learning-anaconda/
-
-## Installationsueberpruefung und Laden vortrainierter Modelle
-
-Zur Ueberpruefung das alle Pakete korrekt installiert sind, ladet bitte das folgende Jupyter-Notebook herunter und testet ob alle Zeilen kompilieren. Achtung, mit der Ausfuehrung der Zellen werden gleichzeitig vortrainierte Netzwerke von ca. 600 MB heruntergeladen:
+Zur Überpruefung das alle Pakete korrekt installiert sind, ladet bitte das folgende Jupyter-Notebook herunter und testet ob alle Zeilen kompilieren. Achtung, mit der Ausfuehrung der Zellen werden gleichzeitig vortrainierte Netzwerke von ca. 600 MB heruntergeladen:
 
 https://raw.githubusercontent.com/Christian-Hirsch/kusanus/master/tech_check.ipynb
 
+## Notebooks
 
-## Datensaetze
-
-Wir werden weiterhin einige grosse Datensaetze zur Auswertung benoetigen. Naehere Details folgen in einer E-Mail ca.
-eine Woche vor Workshopbeginn.
+Die im Workshop verwendeten [Notebooks](https://github.com/Christian-Hirsch/kusanus/blob/master/notebooks) basieren auf dem exzellenten Kurs [Practical Deep Learning For Coders](http://course.fast.ai/) von Jeremy Howard. Dort werden eine ganze weitere Reihe von Tricks vorgestellt; ich kann diesen Kurs nur jedem wärmstens ans Herz legen, der tiefer in *Deep Learning* einsteigen möchte!
---
+++ scripts/nn_helper.py
@@ -0,0 +1,76 @@
+from tensorflow.contrib.keras.python.keras.layers import Dense
+from tensorflow.contrib.keras.python.keras.preprocessing import image
+from tensorflow.contrib.keras.python.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.contrib.keras.python.keras.models import Model
+from tensorflow.contrib.keras.python.keras.layers import Dense
+from tensorflow.contrib.keras.python.keras.optimizers import Adam
+from matplotlib import pylab as plt
+
+
+#modified from Jeremy Howard's VGG16BN
+
+def get_batches(path, gen=image.ImageDataGenerator(), shuffle=True, batch_size=8, class_mode='categorical', seed = None):
+    """Retrieve images in batches
+
+    #Arguments
+     path: path to images
+
+    #Returns
+     generator creating data batches
+    """
+    return gen.flow_from_directory(path, target_size=(224, 224),
+            class_mode = class_mode, shuffle = shuffle, batch_size = batch_size, seed = seed)
+
+
+def finetune(base_model, n_class, lr = 1e-3):
+    """Refine a given model by removing the last layer and adding a custom new one
+
+    #Arguments
+     base_model: model to start from
+     n_class: number of classes
+
+    #Returns
+     Model where only the last layer is trained
+    """
+
+    for layer in base_model.layers: layer.trainable = False
+
+    x  = base_model.layers[-2].output
+    x = Dense(n_class, activation = 'softmax')(x)
+
+    model = Model(inputs = base_model.input, outputs = x)
+    model.compile(optimizer = Adam(lr = lr), loss = 'categorical_crossentropy', metrics = ['accuracy'])
+    return model
+
+
+def show_image(path):
+    """Print an image to the notebook
+
+    #Arguments
+     path: location of image
+    """
+    show_array(img_to_array(load_img(path)))
+    
+def show_array(arr, figure = None, cmap = None, figsize = (15, 15)):
+    """Print an image given as array to the notebook
+
+    #Arguments
+     arr: array-image to be printed
+    """
+
+    arr= arr[0] if len(arr.shape) == 4 else arr
+    if figure: 
+        figure.imshow(arr/255)
+    else:
+        plt.figure(figsize = figsize)
+        plt.imshow(arr/255, cmap = cmap)
+
+def show_array_list(arr_list):
+    """Print a list of images as array to the notebook
+
+    #Arguments
+     arr_list: list of arrays
+    """
+    fig, axs = plt.subplots(1, len(arr_list))
+    for arr,ax in zip(arr_list,axs): show_array(arr, ax)
+   
